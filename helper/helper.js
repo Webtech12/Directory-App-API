3fbc03756637b1bf3b8f66777be4e2ab227b9796
Sherwin <sherwinlukes@gmail.com>
2020-03-12 12:18:36 -0700

    adding a helper class

---
+++ helper/helper.js
@@ -0,0 +1,12 @@
+class HelperClass {
+    constructor() {
+        console.log('class instantiated')
+    }
+
+    // static method for url
+    static staticMethodURL() {
+        return '/api/v1';
+    }
+}
+
+module.exports = HelperClass.staticMethodURL()
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const Url = require('../helper/helper')
 
 // Load env vars
 dotenv.config()
@@ -7,5 +8,11 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get(`${Url}/bootcamps`, (req, res) => { res.status(200).send('works') })
+
+
+
 app.listen(port, () => console.log(`App running in ${process.env.NODE_ENV} mode, listening on port ${port}`))
